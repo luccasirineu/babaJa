0a4c21e9622eee6a09d1149d437c3ef7ee1405f7
luccasirineu <luccas19100@outlook.com>
2024-11-13 01:07:34 -0300

    Adicionando Gunicorn ao Procfile

--- app.py
+++ app.py
@@ -212,6 +212,7 @@ def get_users():
     else:
         return jsonify({"message": "Nenhum usuário encontrado"}), 404
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
 
