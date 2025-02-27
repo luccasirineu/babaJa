057b299821b50df673aca37ffc814dbb021f0b5f
Luccas Gonçalves Irineu <luccas19100@outlook.com>
2025-02-27 00:37:09 -0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="137.184.82.206",
-        user="user_admin",
-        password="1910",
-        database="mydatabase"
-    )
